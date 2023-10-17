eda1c7a6bc63cc2dbf3df9faf05891d43383865b
baschenbrenner <baschenbrenner@hotmail.com>
2023-10-17 18:03:28 -0500

    fixed home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -5,6 +5,7 @@ const Home = ({ user }) => {
         <div id='welcomePage'>
             <h1 id='Welcome'>Welcome {user.name}</h1>
             <p className='WelcomeP' >Welcome to the Equity Learning Community Web Application!</p>
+            <p> The purpose of this app is to help connect practitioners across time and space. We are doing very similar work but it is easy to lose track of our equity colleagues. We are joined together through this AMATYC learning community which is supported through this web application! Welcome!</p>
             
             
         </div>
